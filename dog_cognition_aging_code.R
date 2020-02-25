97f0608905fbf261c5818ea5b55a42a1302f6485
mwatowich <49698282+mwatowich@users.noreply.github.com>
2020-02-24 23:50:36 -0800

    Update dog_cognition_aging_code.R

--- dog_cognition_aging_code.R
+++ dog_cognition_aging_code.R
@@ -12,17 +12,17 @@ library(EMMREML)
 #     Load data ---------------
 
 # Prep cognitive and demographic data
-dog_scores <- as.data.frame(read_xlsx("Supplemental_Material_dog_cognition_aging.xlsx", sheet = 9, col_names = T, range = cell_rows(2:12))) %>% 
+dog_scores <- as.data.frame(read_xlsx("ESM_2_dog_cognitive_aging.xlsx", sheet = 9, col_names = T, range = cell_rows(2:12))) %>% 
   rename(sex = `sex*`) %>% 
   rename(reproductive.alteration = `reproductive.alteration†`)
 
 # Prep identity by state matrix
-IBS <- as.matrix(read_xlsx("Supplemental_Material_dog_cognition_aging.xlsx", sheet = 10, col_names = T, range = cell_limits(c(2,2), c(11,10))))
+IBS <- as.matrix(read_xlsx("ESM_2_dog_cognitive_aging.xlsx", sheet = 10, col_names = T, range = cell_limits(c(2,2), c(11,10))))
 row.names(IBS) <-colnames(IBS)
 colnames(IBS) <- NULL
 
 # Prep Zmatrix
-Zmatrix <- as.matrix(read_xlsx("Supplemental_Material_dog_cognition_aging.xlsx", sheet = 11, col_names = F, range = cell_limits(c(3,1), c(12,9))))
+Zmatrix <- as.matrix(read_xlsx("ESM_2_dog_cognitive_aging.xlsx", sheet = 11, col_names = F, range = cell_limits(c(3,1), c(12,9))))
 rownames(Zmatrix) <- seq(1:nrow(Zmatrix))
 
 
@@ -42,35 +42,41 @@ total.counts <- as.data.frame(rbind(matrix(nrow = length(dog_scores$arm.pointing
 colnames(total.counts) <- 'total.counts'
 
 # Covariates file
-covariates <- dog_scores %>% select(sex, reproductive.alteration, mean.breed.lifespan, age) #select model covariates 
+covariates <- dog_scores %>% 
+  select(sex, reproductive.alteration, mean.breed.lifespan, age) #select model covariates 
 
 # Model matrix
-mat_coeffs <- model.matrix(~ sex + reproductive.alteration + scale(mean.breed.lifespan) + poly(scale(age),2), data= covariates)
+mat_coeffs <- model.matrix(~ sex + 
+                             reproductive.alteration + 
+                             scale(mean.breed.lifespan) + 
+                             poly(scale(age),2), 
+                           data= covariates)
 
 # Model binomial cognitive abilities 
-out <- pqlseq(RawCountDataSet = t(success.counts), Phenotypes = mat_coeffs[,2], Covariates =  mat_coeffs[,-2], RelatednessMatrix = GRM, LibSize = t(total.counts), fit.model = 'BMM', fit.method = 'REML', fit.maxiter = 1000, fit.tol = 1e-5, filtering = FALSE, verbose = FALSE) # this line shows the set-up for using PQLseq to run a binimial mixed model with the relatedness matrix, but model with toy data will not converge
+out <- pqlseq(RawCountDataSet = t(success.counts), 
+              Phenotypes = mat_coeffs[,2], 
+              Covariates =  mat_coeffs[,-2], 
+              RelatednessMatrix = GRM, 
+              LibSize = t(total.counts), 
+              fit.model = 'BMM', 
+              fit.method = 'REML') # this shows the set-up for using PQLseq to run a binimial mixed model with the relatedness matrix. Please note that model with toy data will not converge
 
 
 
-#      Generate files to model delay of gratification with EMMREML package ---------------
+#      Generate files to model delay of gratification (or eye contact) ---------------
 dog_delay_grat <- dog_scores %>% select(`delay.gratification.watching(s)`, `delay.gratification.eyes.closed(s)`, `delay.gratification.turn.back(s)`) # select only the delay of gratification tasks 
 
-# Run Principal Component Analysis
+# Principal Component Analysis
 dogs_pca <- prcomp(dog_delay_grat, center = T, scale = T) # run PCA
-summary(dogs_pca) 
 pca_vals <- as.data.frame(dogs_pca$x) #PCA scores for each observation
 dog_delay_grat$PC1 <- pca_vals$PC1 # add PCA values to the dataframe
 
-# Run model
+# Run model 
 model.delay.grat <- emmreml(y = dog_delay_grat$PC1, # values of 1st PCA
                         X = mat_coeffs,  # design matrix
                         Z = Zmatrix,  # matrix of random effects
                         K = IBS,  # relatedness matrix
                         varbetahat = T, varuhat = T, PEVuhat = T, test = T)
 
-delay.grat.out <- as.data.frame(as.data.frame(model.delay.grat$pvalbeta)$none, ncol = 1); colnames(delay.grat.out) <- "pvals"  # store p-values as object
-delay.grat.out$betas <- model.delay.grat$betahat
-delay.grat.out$AIC <- 2*(ncol(mat_coeffs) -1) - 2*model.delay.grat$loglik # calculate AIC
-
-
-
+delay.grat.out <- cbind(model.delay.grat$betahat, model.delay.grat$pvalbeta[, "none"])
+colnames(delay.grat.out) <- c("p-values", "beta coefficients")
