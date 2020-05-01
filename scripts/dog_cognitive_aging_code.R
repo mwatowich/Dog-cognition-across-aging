c63bc6ed7c2ef27a02df915fb0d04dad80509c0c
mwatowich <49698282+mwatowich@users.noreply.github.com>
2020-05-01 14:30:53 -0700

    Update dog_cognitive_aging_code.R

--- scripts/dog_cognitive_aging_code.R
+++ scripts/dog_cognitive_aging_code.R
@@ -11,21 +11,50 @@ library(EMMREML)
 # Download the example IBD matrix, Z matrix, and data from the Github page mwatowich/Dog-cognition-across-aging 
 
 # Load cognitive and demographic data
-dog_scores <- read_csv(file = "example_data_dog_cognitive_aging.csv", 
+dog_scores <- read_csv(file = "~/Downloads/Dog-cognition-across-aging-master/scripts/example_data_dog_cognitive_aging.csv", 
                        col_names = T, n_max = 10) %>% as.data.frame()
 colnames(dog_scores)[c(4,5)] <- c("sex", "reproductive.alteration")
 
 # Load identity by decent matrix
-IBD <- read_delim(file = "example_IBDmatrix_dog_cognitive_aging.txt", 
+IBD <- read_delim(file = "~/Downloads/Dog-cognition-across-aging-master/scripts/example_IBDmatrix_dog_cognitive_aging.txt", 
                   delim = "\t", 
                   col_names = T) %>% 
   column_to_rownames("X1") %>% 
   as.matrix()
 
 # Load Zmatrix
-Zmatrix <- as.matrix(read_csv("example_Zmatrix_dog_cognitive_aging.csv", 
+Zmatrix <- as.matrix(read_csv("~/Downloads/Dog-cognition-across-aging-master/scripts/example_Zmatrix_dog_cognitive_aging.csv", 
                               col_names = T))
 rownames(Zmatrix) <- seq(1:nrow(Zmatrix))
+
+
+#      Make model matrices to test equations 1-4 from Watowich et al. 2020 ---------------
+# Model matrix with all covariates and age as a linear function (Eq 1)
+linear_cov <- model.matrix(~ sex + 
+                             reproductive.alteration +
+                             scale(mean.breed.lifespan) + 
+                             scale(age),
+                           data= dog_scores)
+
+# Model matrix with age as a logarithmic function (Eq 2)
+log_cov <- model.matrix(~ sex + 
+                          reproductive.alteration +
+                          scale(mean.breed.lifespan) + 
+                          log(2 + scale(age)), 
+                        data= dog_scores)
+
+# Model matrix of the truncation hypothesis (additive; Eq 3)
+truncation_cov <- model.matrix(~ sex + 
+                                 reproductive.alteration +
+                                 scale(mean.breed.lifespan) + 
+                                 poly(scale(age),2),
+                               data= dog_scores)
+
+# Model matrix of the compression hypothesis (interactive; Eq 4)
+compression_cov <- model.matrix(~ sex + 
+                                  reproductive.alteration +
+                                  scale(mean.breed.lifespan)*poly(scale(age),2), 
+                                data= dog_scores)
 
 
 #      Model binomial cognitive tasks with PQLseq package ---------------
@@ -45,25 +74,12 @@ total.counts <- as.data.frame(rbind(matrix(nrow = length(dog_scores$arm.pointing
                                            data= rep(as.integer(6)))))
 colnames(total.counts) <- c('total.counts')     # change column names 
 
-# Model matrix of the truncation hypothesis (additive)
-truncation_cov <- model.matrix(~ sex + 
-                                 reproductive.alteration +
-                                 scale(mean.breed.lifespan) + 
-                                 poly(scale(age),2),
-                               data= dog_scores)
-
-# Model matrix of the compression hypothesis (interactive)
-compression_cov <- model.matrix(~ sex + 
-                                  reproductive.alteration +
-                                  scale(mean.breed.lifespan)*poly(scale(age),2), 
-                                data= dog_scores)
-
 
 # Model binomial tasks of cognitive performance
 # This shows the set-up for using PQLseq to run a binimial mixed model with a genetic relatedness matrix 
 # Please note that models with example data will not converge 
 
-# Model of truncation hypothesis 
+# Example: Model of the truncation hypothesis 
 truncation_out <- pqlseq(RawCountDataSet = t(success.counts), 
                          Phenotypes = truncation_cov[,2],    # choose one covariate to set as the "phenotype" 
                          Covariates =  truncation_cov[,-2],  # set all other covariates as "covariates" 
@@ -72,16 +88,10 @@ truncation_out <- pqlseq(RawCountDataSet = t(success.counts),
                          fit.model = 'BMM', 
                          fit.method = 'REML') 
 
-# Model of compression hypothesis 
-compression_out <- pqlseq(RawCountDataSet = t(success.counts), 
-                         Phenotypes = compression_cov[,2],    
-                         Covariates =  compression_cov[,-2], 
-                         RelatednessMatrix = GRM, 
-                         LibSize = t(total.counts), 
-                         fit.model = 'BMM', 
-                         fit.method = 'REML') 
+# Model the linear, log, truncation, and compression models (Eq 1-4) as shown above 
 
-# calculate AIC from loglik output, compare AIC of the truncation and compression models 
+# Calculate AIC from loglik output, compare AIC of the linear, log, and quadratic models (Eq 1-3) 
+# Calculate AIC from loglik output, compare AIC of the truncation and compression models (Eq 3-4)
 
 
 #      Model delay of gratification (or eye contact) with EMMREML ---------------
@@ -92,36 +102,26 @@ dog_delay_grat <- dog_scores %>%
        `delay.gratification.turn.back(s)`) 
 
 # Principal Component Analysis
-dogs_pca <- prcomp(dog_delay_grat, center = T, scale = T)                 # perform PCA
-pca_vals <- as.data.frame(dogs_pca$x)                                     # PCA scores for each observation
-dog_delay_grat$PC1 <- pca_vals$PC1                                        # add PC1 values to the dataframe
+dogs_pca <- prcomp(dog_delay_grat, center = T, scale = T)   # perform PCA
+pca_vals <- as.data.frame(dogs_pca$x)                       # PCA scores for each observation
+dog_delay_grat$PC1 <- pca_vals$PC1                          # add PC1 values to the dataframe
 
 
-# Model of truncation hypothesis 
-truncation_delay_grat <- emmreml(y = dog_delay_grat$PC1,                  # values of 1st PCA
-                        X = truncation_cov,                               # covariates 
-                        Z = Zmatrix,                                      # matrix of random effects
-                        K = IBD,                                          # relatedness matrix
+# Example: Model of the truncation hypothesis 
+truncation_delay_grat <- emmreml(y = dog_delay_grat$PC1,    # values of 1st PCA
+                        X = truncation_cov,                 # covariates 
+                        Z = Zmatrix,                        # matrix of random effects
+                        K = IBD,                            # relatedness matrix
                         varbetahat = T, varuhat = T, PEVuhat = T, test = T)
 
-truncation_delay_grat <- cbind(truncation_delay_grat$betahat,             # betas
-                               truncation_delay_grat$varbetahat,          # se
-                               truncation_delay_grat$pvalbeta[, "none"],  # p-values 
-                               truncation_delay_grat$loglik)              # log likelihood
+truncation_delay_grat <- cbind(truncation_delay_grat$betahat, 
+                               truncation_delay_grat$varbetahat, 
+                               truncation_delay_grat$pvalbeta[, "none"], 
+                               truncation_delay_grat$loglik)
+
 colnames(truncation_delay_grat) <- c("betas", "se", "p-values", "loglik")
 
+# Model the linear, log, truncation, and compression models (Eq 1-4) as shown above 
 
-# Model of compression hypothesis 
-compression_delay_grat <- emmreml(y = dog_delay_grat$PC1,
-                                 X = compression_cov,
-                                 Z = Zmatrix,
-                                 K = IBD,
-                                 varbetahat = T, varuhat = T, PEVuhat = T, test = T)
-
-compression_delay_grat <- cbind(compression_delay_grat$betahat, 
-                               compression_delay_grat$varbetahat, 
-                               compression_delay_grat$pvalbeta[, "none"], 
-                               compression_delay_grat$loglik)
-colnames(compression_delay_grat) <- c("betas", "se", "p-values", "loglik")
-
-# calculate AIC from loglik output, compare AIC of the truncation and compression models 
+# Calculate AIC from loglik output, compare AIC of the linear, log, and quadratic models (Eq 1-3) 
+# Calculate AIC from loglik output, compare AIC of the truncation and compression models (Eq 3-4)
