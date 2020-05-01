67c0f36835d33ceac9200cfc25a686aff80678fd
mwatowich <49698282+mwatowich@users.noreply.github.com>
2020-05-01 14:06:56 -0700

    Update dog_cognitive_aging_code.R

--- scripts/dog_cognitive_aging_code.R
+++ scripts/dog_cognitive_aging_code.R
@@ -8,14 +8,12 @@ library(EMMREML)
 
 
 #     Load data ---------------
-# Download the toy IBD matrix, Z matrix, and data from the Github page mwatowich/Dog-cognition-across-aging/scripts.
+# Download the example IBD matrix, Z matrix, and data from the Github page mwatowich/Dog-cognition-across-aging 
 
 # Load cognitive and demographic data
 dog_scores <- read_csv(file = "example_data_dog_cognitive_aging.csv", 
-                       col_names = T, n_max = 10) %>% 
-  rename(sex = `sex*`) %>% 
-  rename(reproductive.alteration = `reproductive.alteration†`) %>% 
-  as.data.frame()
+                       col_names = T, n_max = 10) %>% as.data.frame()
+colnames(dog_scores)[c(4,5)] <- c("sex", "reproductive.alteration")
 
 # Load identity by decent matrix
 IBD <- read_delim(file = "example_IBDmatrix_dog_cognitive_aging.txt", 
@@ -32,8 +30,8 @@ rownames(Zmatrix) <- seq(1:nrow(Zmatrix))
 
 #      Model binomial cognitive tasks with PQLseq package ---------------
 # Genetic relatedness matrix
-GRM = Zmatrix%*%IBD%*%t(Zmatrix) # multiply Zmatrix and IBD matrices to create genetic relatedness matrix
-diag(GRM)=1 # set relatedness of the individual to 1
+GRM = Zmatrix%*%IBD%*%t(Zmatrix)                # multiply Zmatrix and IBD matrices to create genetic relatedness matrix
+diag(GRM)=1                                     # set relatedness of the individual to 1
 
 # Success counts
 success.counts <- as.data.frame(matrix(nrow = length(dog_scores$arm.pointing), 
@@ -45,52 +43,85 @@ colnames(success.counts) <- c('success.counts') # change column names
 total.counts <- as.data.frame(rbind(matrix(nrow = length(dog_scores$arm.pointing), 
                                            ncol = 1, 
                                            data= rep(as.integer(6)))))
-colnames(total.counts) <- c('total.counts') # change column names 
+colnames(total.counts) <- c('total.counts')     # change column names 
 
-# Covariates file
-covariates <- dog_scores %>% 
-  select(sex, 
-         reproductive.alteration, 
-         mean.breed.lifespan, 
-         age) #select model covariates 
+# Model matrix of the truncation hypothesis (additive)
+truncation_cov <- model.matrix(~ sex + 
+                                 reproductive.alteration +
+                                 scale(mean.breed.lifespan) + 
+                                 poly(scale(age),2),
+                               data= dog_scores)
 
-# Model matrix
-mat_coeffs <- model.matrix(~ sex + 
-                             reproductive.alteration + 
-                             scale(mean.breed.lifespan) + 
-                             poly(scale(age),2), 
-                           data= covariates)
+# Model matrix of the compression hypothesis (interactive)
+compression_cov <- model.matrix(~ sex + 
+                                  reproductive.alteration +
+                                  scale(mean.breed.lifespan)*poly(scale(age),2), 
+                                data= dog_scores)
 
-# Model binomial cognitive performance 
-# This shows the set-up for using PQLseq to run a binimial mixed model with the relatedness matrix 
-# Please note that model with toy data will not converge 
-out <- pqlseq(RawCountDataSet = t(success.counts), 
-              Phenotypes = mat_coeffs[,2], 
-              Covariates =  mat_coeffs[,-2], 
-              RelatednessMatrix = GRM, 
-              LibSize = t(total.counts), 
-              fit.model = 'BMM', 
-              fit.method = 'REML') 
+
+# Model binomial tasks of cognitive performance
+# This shows the set-up for using PQLseq to run a binimial mixed model with a genetic relatedness matrix 
+# Please note that models with example data will not converge 
+
+# Model of truncation hypothesis 
+truncation_out <- pqlseq(RawCountDataSet = t(success.counts), 
+                         Phenotypes = truncation_cov[,2],    # choose one covariate to set as the "phenotype" 
+                         Covariates =  truncation_cov[,-2],  # set all other covariates as "covariates" 
+                         RelatednessMatrix = GRM, 
+                         LibSize = t(total.counts),
+                         fit.model = 'BMM', 
+                         fit.method = 'REML') 
+
+# Model of compression hypothesis 
+compression_out <- pqlseq(RawCountDataSet = t(success.counts), 
+                         Phenotypes = compression_cov[,2],    
+                         Covariates =  compression_cov[,-2], 
+                         RelatednessMatrix = GRM, 
+                         LibSize = t(total.counts), 
+                         fit.model = 'BMM', 
+                         fit.method = 'REML') 
+
+# calculate AIC from loglik output, compare AIC of the truncation and compression models 
 
 
 #      Model delay of gratification (or eye contact) with EMMREML ---------------
 # Select only the delay of gratification tasks 
 dog_delay_grat <- dog_scores %>% 
-select(`delay.gratification.watching(s)`, 
+  dplyr::select(`delay.gratification.watching(s)`, 
        `delay.gratification.eyes.closed(s)`, 
        `delay.gratification.turn.back(s)`) 
 
 # Principal Component Analysis
-dogs_pca <- prcomp(dog_delay_grat, center = T, scale = T) # run PCA
-pca_vals <- as.data.frame(dogs_pca$x) #PCA scores for each observation
-dog_delay_grat$PC1 <- pca_vals$PC1 # add PCA values to the dataframe
+dogs_pca <- prcomp(dog_delay_grat, center = T, scale = T)                 # perform PCA
+pca_vals <- as.data.frame(dogs_pca$x)                                     # PCA scores for each observation
+dog_delay_grat$PC1 <- pca_vals$PC1                                        # add PC1 values to the dataframe
 
-# Run model 
-model.delay.grat <- emmreml(y = dog_delay_grat$PC1, # values of 1st PCA
-                        X = mat_coeffs,  # design matrix
-                        Z = Zmatrix,  # matrix of random effects
-                        K = IBD,  # relatedness matrix
+
+# Model of truncation hypothesis 
+truncation_delay_grat <- emmreml(y = dog_delay_grat$PC1,                  # values of 1st PCA
+                        X = truncation_cov,                               # covariates 
+                        Z = Zmatrix,                                      # matrix of random effects
+                        K = IBD,                                          # relatedness matrix
                         varbetahat = T, varuhat = T, PEVuhat = T, test = T)
 
-delay.grat.out <- cbind(model.delay.grat$betahat, model.delay.grat$pvalbeta[, "none"])
-colnames(delay.grat.out) <- c("p-values", "betas")
+truncation_delay_grat <- cbind(truncation_delay_grat$betahat,             # betas
+                               truncation_delay_grat$varbetahat,          # se
+                               truncation_delay_grat$pvalbeta[, "none"],  # p-values 
+                               truncation_delay_grat$loglik)              # log likelihood
+colnames(truncation_delay_grat) <- c("betas", "se", "p-values", "loglik")
+
+
+# Model of compression hypothesis 
+compression_delay_grat <- emmreml(y = dog_delay_grat$PC1,
+                                 X = compression_cov,
+                                 Z = Zmatrix,
+                                 K = IBD,
+                                 varbetahat = T, varuhat = T, PEVuhat = T, test = T)
+
+compression_delay_grat <- cbind(compression_delay_grat$betahat, 
+                               compression_delay_grat$varbetahat, 
+                               compression_delay_grat$pvalbeta[, "none"], 
+                               compression_delay_grat$loglik)
+colnames(compression_delay_grat) <- c("betas", "se", "p-values", "loglik")
+
+# calculate AIC from loglik output, compare AIC of the truncation and compression models 
